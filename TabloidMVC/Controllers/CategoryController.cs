ed292a221408a5b5ee802cc7abc67a0d20b477ea
noahnicholson <noahnicholson239@yahoo.com>
2022-09-27 15:31:15 -0500

    category

---
+++ TabloidMVC/Controllers/CategoryController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using TabloidMVC.Models;
+using TabloidMVC.Models.ViewModels;
+using TabloidMVC.Repositories;
+
+
+
+namespace TabloidMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryController(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+        public IActionResult Index()
+        {
+            var categories = _categoryRepository.GetAll();
+            return View(categories);
+        }
+
+        public IActionResult Create()
+        {
+            var cat = new Category();
+            var Categories = _categoryRepository.GetAll();
+            return View(cat);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            try
+            {
+
+                category.Name = category.Name;
+                _categoryRepository.AddCategory(category);
+
+                return RedirectToAction("Index", new { id = category.Id });
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
+
+  
+        
--- TabloidMVC/Repositories/CategoryRepository.cs
+++ TabloidMVC/Repositories/CategoryRepository.cs
@@ -33,6 +33,60 @@ namespace TabloidMVC.Repositories
                     return categories;
                 }
             }
+
+        }
+        public void AddCategory(Category category)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        INSERT INTO Category (
+                          Name )
+                        OUTPUT INSERTED.ID
+                        VALUES (
+                            @Name )";
+                    cmd.Parameters.AddWithValue("@Name", category.Name);
+
+                    category.Id = (int)cmd.ExecuteScalar();
+                }
+            }
+        }
+
+        public Category GetCategoryById(int id)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                            SELECT Id, [Name] FROM Category
+                            WHERE Id = @id";
+
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            Category category = new Category
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                Name = reader.GetString(reader.GetOrdinal("Name")),
+                            };
+                            return category;
+                        }
+                        else
+                        {
+                            return null;
+                        }
+                    }
+                }
+            }
         }
     }
 }
